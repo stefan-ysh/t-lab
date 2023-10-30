3422130d27c9a8493cba6c6062a9dc79bf325a00
yshuai <stefan_ysh@foxmail.com>
2023-10-30 22:55:06 +0800

    fix:build failed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["nuxt-primevue"],
+  // modules: ["nuxt-primevue"],
   css: [
     "~/assets/css/tailwind.css",
-    "primevue/resources/themes/lara-dark-teal/theme.css",
-    "primeicons/primeicons.css",
+    // "primevue/resources/themes/lara-dark-teal/theme.css",
+    // "primeicons/primeicons.css",
   ],
   app: {
     pageTransition: { name: "page", mode: "out-in" },

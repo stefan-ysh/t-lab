1524415559f757ea4d129dc2b4f4d6897523a82b
yshuai <stefan_ysh@foxmail.com>
2023-11-01 22:52:51 +0800

    feat:增加中间工作室布局

--- pages/index.vue
+++ pages/index.vue
@@ -60,147 +60,27 @@
                 Know More
             </div>
         </div>
-        <div class="page-container">
-            <section class="section-container no-bg-container lg:pr-[15rem] bg-default [&>h2]:h3 bg-white text-black">
-                <h2 class=" mt-10 mb-20 ml-20">
-                    We solve human needs through the power of human intellect. As Nokia's industrial research lab, we
-                    innovate
-                    with purpose, pursuing responsible, sustainable technologies that will have a demonstrable impact on
-                    society.
-
-                </h2>
-            </section>
-            <section class="section-bg-container lg:min-h-[43.75rem] min-h-[23.75rem] lg:flex sm:block">
-                <div class="lg:w-6/12 sm:w-full">
-                    <img src="@/assets/img/index_1.png" class="object-cover w-full h-full">
-                </div>
-                <div
-                    class="lg:w-6/12 sm:w-full w-full bg-slate-400 flex flex-col justify-center lg:p-10 sm:p-10 p-10 text-black sm:mt-10 lg:mt-0">
-                    <h3 class="pb-10">
-                        Our Research
-                    </h3>
-                    <h4 class="w-[105%] py-10">
-                        Nokia Bell Labs is innovating the technologies that will transform the way we live our lives
-                    </h4>
-                    <div class="flex items-center">
-                        <a href="#">Learn about our four research areas</a>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true"
-                            class="md:h-12 md:w-12 h-10 w-10 p-2 rounded-full border-[1.5px] text-black">
-                            <path fill-rule="evenodd"
-                                d="M16.28 11.47a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 01-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 011.06-1.06l7.5 7.5z"
-                                clip-rule="evenodd"></path>
-                        </svg>
-                    </div>
-                </div>
-            </section>
-            <section class="section-bg-container lg:flex sm:block p-10 pb-0">
-                <img src="@/assets/img/index_2.jpg"
-                    class=" lg:w-6/12 sm:w-full px-10object-contain w-full h-full rounded-md m-auto">
-                <div class="lg:w-6/12 sm:w-full flex flex-col justify-center lg:p-10 sm:px-0 sm:py-10 text-black">
-                    <a href="#"
-                        class="gta-featured-topics lg:row-start-1 lg:col-span-2 col-span-1 flex flex-col gap-y-content-base hover-scale">
-                        <h3>Responsible AI</h3>
-                        <div class="flex gap-x-content-sm items-center">
-                            <h4 class="flex-1 line-clamp-4">For AI to thrive, it must put humans first</h4><svg
-                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                                aria-hidden="true" class="h-9 w-9 md:h-12 md:w-12 p-1 rounded-full border-[1.5px]">
-                                <path fill-rule="evenodd"
-                                    d="M16.28 11.47a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 01-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 011.06-1.06l7.5 7.5z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
-                        </div>
-                    </a>
-                    <div class="lg:flex items-center sm:block">
-                        <a href="#"
-                            class=" hover:scale-105 transition-all justify-center sm:w-full sm:mx-0 gta-featured-topics flex p-6 m-6 w-full mx-0 flex-col p-content-base gap-y-content-sm rounded-md bg-gray-200 h-full hover-scale">
-                            <p class="caption-base text-lg">Research</p>
-                            <div class="flex md:flex-col flex-row gap-content-sm md:items-start items-center">
-                                <h5 class="flex-1 line-clamp-3">What is Responsible AI?</h5>
-                            </div>
-                        </a>
-                        <a href="#"
-                            class=" hover:scale-105 transition-all justify-center sm:w-full sm:mx-0 lg:mx-10 gta-featured-topics flex p-6 m-6 w-full mx-0 flex-col p-content-base gap-y-content-sm rounded-md bg-gray-200 h-full hover-scale">
-                            <p class="caption-base text-lg">Blog</p>
-                            <div class="flex md:flex-col flex-row gap-content-sm md:items-start items-center">
-                                <h5 class="flex-1 line-clamp-3">AI solutions look to create a sustainable future</h5>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            </section>
-            <section class="section-bg-container lg:flex sm:block mb-10 p-10 pt-0">
-                <div class="lg:w-6/12 sm:w-full flex flex-col justify-center lg:p-10 sm:px-0 sm:py-0 text-black">
-                    <a href="#"
-                        class="gta-featured-topics lg:row-start-1 lg:col-span-2 col-span-1 flex flex-col gap-y-content-base hover-scale">
-                        <h3>Responsible AI</h3>
-                        <div class="flex gap-x-content-sm items-center">
-                            <h4 class="flex-1 line-clamp-4">For AI to thrive, it must put humans first</h4><svg
-                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                                aria-hidden="true" class="h-9 w-9 md:h-12 md:w-12 p-1 rounded-full border-[1.5px]">
-                                <path fill-rule="evenodd"
-                                    d="M16.28 11.47a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 01-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 011.06-1.06l7.5 7.5z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
-                        </div>
-                    </a>
-                    <div class="lg:flex items-center sm:block">
-                        <a href="#"
-                            class=" hover:scale-105 transition-all justify-center sm:w-full sm:mx-0 w-full mx-0 gta-featured-topics flex p-6 m-6  flex-col p-content-base gap-y-content-sm rounded-md bg-gray-200 h-full hover-scale">
-                            <p class="caption-base text-lg">Research</p>
-                            <div class="flex md:flex-col flex-row gap-content-sm md:items-start items-center">
-                                <h5 class="flex-1 line-clamp-3">What is Responsible AI?</h5>
-                            </div>
-                        </a>
-                        <a href="#"
-                            class=" hover:scale-105 transition-all justify-center sm:w-full sm:mx-0 w-full mx-0 lg:mx-10 gta-featured-topics flex p-6 m-6  flex-col p-content-base gap-y-content-sm rounded-md bg-gray-200 h-full hover-scale">
-                            <p class="caption-base text-lg">Blog</p>
-                            <div class="flex md:flex-col flex-row gap-content-sm md:items-start items-center">
-                                <h5 class="flex-1 line-clamp-3">AI solutions look to create a sustainable future</h5>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <img src="@/assets/img/index_3.jpg"
-                    class=" lg:w-6/12 sm:w-full px-10object-contain w-full h-full rounded-md m-auto">
-
-            </section>
-            <section class="section-bg-container lg:flex sm:block p-10 py-0">
-                <img src="@/assets/img/index_4.jpg"
-                    class=" lg:w-6/12 sm:w-full px-10object-contain w-full h-full rounded-md m-auto">
-                <div class="lg:w-6/12 sm:w-full flex flex-col justify-center lg:p-10 sm:px-0 sm:py-10 text-black">
-                    <a href="#"
-                        class="gta-featured-topics lg:row-start-1 lg:col-span-2 col-span-1 flex flex-col gap-y-content-base hover-scale">
-                        <h3>UNEXT</h3>
-                        <div class="flex gap-x-content-sm items-center">
-                            <h4 class="flex-1 line-clamp-4">UNIX transformed computing; Now it’s UNEXT’s turn to transform
-                                networking</h4><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-                                fill="currentColor" aria-hidden="true"
-                                class="h-9 w-9 md:h-12 md:w-12 p-1 rounded-full border-[1.5px]">
-                                <path fill-rule="evenodd"
-                                    d="M16.28 11.47a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 01-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 011.06-1.06l7.5 7.5z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
-                        </div>
-                    </a>
-                    <div class="lg:flex items-center sm:block">
-                        <a href="#"
-                            class=" hover:scale-105 transition-all justify-center sm:w-full w-full sm:mx-0 mx-0 gta-featured-topics flex p-6 m-6  flex-col p-content-base gap-y-content-sm rounded-md bg-gray-200 h-full hover-scale">
-                            <p class="caption-base text-lg">Research</p>
-                            <div class="flex md:flex-col flex-row gap-content-sm md:items-start items-center">
-                                <h5 class="flex-1 line-clamp-3">What is UNEXT?</h5>
-                            </div>
-                        </a>
-                        <a href="#"
-                            class=" hover:scale-105 transition-all justify-center sm:w-full w-full sm:mx-0 mx-0 lg:mx-10 gta-featured-topics flex p-6 m-6  flex-col p-content-base gap-y-content-sm rounded-md bg-gray-200 h-full hover-scale">
-                            <p class="caption-base text-lg">Project</p>
-                            <div class="flex md:flex-col flex-row gap-content-sm md:items-start items-center">
-                                <h5 class="flex-1 line-clamp-3">L4S, the crucial enabler for large-scale deployments of
-                                    real-time applications</h5>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            </section>
+        <div class="studio-container grid grid-cols-3 justify-items-center p-5">
+            <div class="flex m-5 flex-col w-1/3 justify-center items-center">
+                <img class="w-full block h-auto" src="http://fakeimg.pl/2000x800/0079D8/fff/?text=.">
+                <h1 class="absolute">Without</h1>
+            </div>
+            <div class="flex m-5 flex-col w-1/3 justify-center items-center">
+                <img class="w-full block h-auto" src="http://fakeimg.pl/2000x800/0079D8/fff/?text=.">
+                <h1 class="absolute">Studio</h1>
+            </div>
+            <div class="flex m-5 flex-col w-1/3 justify-center items-center">
+                <img class="w-full block h-auto" src="http://fakeimg.pl/2000x800/0079D8/fff/?text=.">
+                <h1 class="absolute">Mine</h1>
+            </div>
+            <div class="flex m-5 flex-col w-1/3 justify-center items-center">
+                <img class="w-full block h-auto" src="http://fakeimg.pl/2000x800/0079D8/fff/?text=.">
+                <h1 class="absolute">Yours</h1>
+            </div>
+            <div class="flex m-5 flex-col w-1/3 justify-center items-center">
+                <img class="w-full block h-auto" src="http://fakeimg.pl/2000x800/0079D8/fff/?text=.">
+                <h1 class="absolute">China</h1>
+            </div>
         </div>
 
         <!-- <h1>index page</h1> -->
@@ -225,23 +105,6 @@ const counter = useState('counter', () => 500)
     align-items: flex-start;
     justify-content: center;
     padding: 1.875rem 1.5rem 3.75rem;
-}
-
-@media (min-width: 768px) {
-    .page-container {
-        row-gap: 5rem;
-        padding-top: 5rem;
-    }
-}
-
-.page-container {
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    row-gap: 3.75rem;
-    --tw-bg-opacity: 1;
-    background-color: rgb(255 255 255 / var(--tw-bg-opacity));
-    padding-top: 3.75rem;
 }
 
 h3 {

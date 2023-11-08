fa47d3789121b5daf0b915f4d8f43acd89f3cfc8
yshuai <stefan_ysh@foxmail.com>
2023-11-08 20:17:08 +0800

    Refactor carousel and studio list components

--- components/second-carousel.vue
+++ components/second-carousel.vue
@@ -1,29 +1,37 @@
 <template>
-    <div class="w-full h-full lg:h-56 flex bg-slate-500 lg:px-24 p-x-0 flex-col lg:flex-row justify-center bg-secondBanner">
-        <div
-            class="test w-full lg:mt-0 mt-10 lg:w-1/3 border-blue-100 h-80 lg:h-auto bg-cover bg-secondImgOne flex flex-col justify-center items-center">
-            <h1 class="mb-10 text-2xl text-cyan-50">Commercial</h1>
-            <button class="btn btn-warning">More Info</button>
-        </div>
-        <div
-            class="test w-full lg:mt-0 mt-10 lg:w-1/3 border-blue-100 h-80 lg:h-auto bg-cover bg-secondImgTwo flex flex-col justify-center items-center ">
-            <h1 class="mb-10 text-2xl text-cyan-50">Industrial</h1>
-            <button class="btn btn-warning">More Info</button>
-        </div>
-        <div
-            class="test w-full lg:mt-0 mt-10 lg:w-1/3 border-blue-100 h-80 lg:h-auto bg-cover bg-secondImgThree flex flex-col justify-center items-center ">
-            <h1 class="mb-10 text-2xl text-cyan-50">Residential</h1>
+    <div class="w-full h-full lg:h-56 flex bg-slate-500 lg:px-6 p-x-0 flex-col lg:flex-row justify-center bg-secondBanner">
+        <div v-for="(carousel, i) in carouselList" :key="carousel.img"
+            class="carousel-item w-full lg:mt-0 mt-10 lg:w-1/3 border-blue-100 h-80 lg:h-auto bg-cover flex flex-col justify-center items-center "
+            :class="carousel.img">
+            <h1 class="mb-10 text-2xl text-cyan-50">{{ carousel.title }}</h1>
             <button class="btn btn-warning">More Info</button>
         </div>
     </div>
 </template>
+<script setup>
+const carouselList = [
+    {
+        title: 'Commercial',
+        img: 'bg-secondImgOne'
+    },
+    {
+        title: 'Industrial',
+        img: 'bg-secondImgTwo'
+    },
+    {
+        title: 'Residential',
+        img: 'bg-secondImgThree'
+    }
+]
+</script>
 <style scoped>
-.test {
+.carousel-item {
     clip-path: polygon(20% 0, 100% 0, 80% 100%, 0 100%);
 }
 
 @media screen and (max-width: 1024px) {
-    .test {
+    .carousel-item {
         clip-path: none;
     }
-}</style>
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -20,7 +20,7 @@
       so you can use the :checked selector on it.
       -->
       <input type="checkbox" />
-      
+
       <!--
       Some spans to act as a hamburger.
       
@@ -30,7 +30,7 @@
       <span></span>
       <span></span>
       <span></span>
-      
+
       <!--
       Too bad the menu has to be inside of the button
       but hey, it's pure CSS magic.
@@ -44,27 +44,17 @@
           </a>
         </template>
       </ul>
-      <!-- <ul id="menu" class="w-6/12 bg-header-color table rounded-xl pl-3 pr-3 whitespace-nowrap my-0 mx-auto text-center">
-        <li v-for="menu in  MENU_LIST" :key="menu.label" class="relative inline-block  ml-12">
-          <a class="block font-sans" :href="'#' ? '#' : '#'" @mouseover="overLi(menu)">{{ menu.label }}</a>
-          <ul v-if="curMenu === menu.label"
-            class="absolute left-1/2 -translate-x-2/4 bg-header-color table my-0 mx-auto rounded-xl p-3">
-            <li v-for="item in menu.items" :key="item.label" class="ml-0 flex flex-col whitespace-nowrap">
-              <a class="relative block font-sans" :href="'#' ? '#' : '#'">{{ item.label }}</a>
-            </li>
-          </ul>
-        </li>
-      </ul> -->
     </div>
   </nav>
   <slot />
   <div class="footer-container w-full md:w-[inherit]" data-v-a9ab81e2="">
-    <div class="lg:block hidden w-full border-t-[1px] border-grey-800 my-10" data-v-a9ab81e2=""></div>
-    <nav class="md:grid hidden grid-cols-5 w-full gap-x-8 p-10" data-v-a9ab81e2="">
+    <div>我们的最新动态</div>
+    <div class="lg:block hidden w-full border-t-[1px] border-grey-800 mb-5" data-v-a9ab81e2=""></div>
+    <nav class="md:grid hidden grid-cols-4 w-full py-5 gap-x-8" data-v-a9ab81e2="">
       <div class="flex flex-col gap-y-content-sm justify-start items-start" data-v-a9ab81e2=""><a
           href="/research-innovation/" class="flex justify-center text-text-primary footer-hover-active"
           data-v-a9ab81e2="">
-          <p class="caption-sm font-bold" data-v-a9ab81e2="">Research Areas</p>
+          <p class="caption-sm font-bold" data-v-a9ab81e2="">植物生长工作室</p>
         </a>
         <div class="flex flex-col gap-y-content-xs justify-start items-start" data-v-a9ab81e2=""><a
             href="/research-innovation/network-fundamentals/"
@@ -83,7 +73,7 @@
       </div>
       <div class="flex flex-col gap-y-content-sm justify-start items-start" data-v-a9ab81e2=""><a href="/institute/"
           class="flex justify-center text-text-primary footer-hover-active" data-v-a9ab81e2="">
-          <p class="caption-sm font-bold" data-v-a9ab81e2="">Publications &amp; Media</p>
+          <p class="caption-sm font-bold" data-v-a9ab81e2="">STEAM 和 PBL工作室</p>
         </a>
         <div class="flex flex-col gap-y-content-xs justify-start items-start" data-v-a9ab81e2=""><a
             href="/institute/blog/" class="flex justify-center text-grey-800 footer-hover-active" data-v-a9ab81e2="">
@@ -102,7 +92,7 @@
       <div class="flex flex-col gap-y-content-sm justify-start items-start" data-v-a9ab81e2=""><a
           href="/bell-labs-consulting/" class="flex justify-center text-text-primary footer-hover-active"
           data-v-a9ab81e2="">
-          <p class="caption-sm font-bold" data-v-a9ab81e2="">Consulting</p>
+          <p class="caption-sm font-bold" data-v-a9ab81e2="">静电纺丝法制备纳米纤维材料设备</p>
         </a>
         <div class="flex flex-col gap-y-content-xs justify-start items-start" data-v-a9ab81e2=""><a
             href="/bell-labs-consulting/communications-service-providers/"
@@ -119,7 +109,7 @@
       <div class="flex flex-col gap-y-content-sm justify-start items-start" data-v-a9ab81e2=""><a
           href="/collaboration-opportunities/" class="flex justify-center text-text-primary footer-hover-active"
           data-v-a9ab81e2="">
-          <p class="caption-sm font-bold" data-v-a9ab81e2="">Collaboration</p>
+          <p class="caption-sm font-bold" data-v-a9ab81e2="">成果：STEAM 和 PBL成果</p>
         </a>
         <div class="flex flex-col gap-y-content-xs justify-start items-start" data-v-a9ab81e2=""><a
             href="/collaboration-opportunities/careers/" class="flex justify-center text-grey-800 footer-hover-active"
@@ -142,36 +132,12 @@
             <p class="caption-sm font-normal" data-v-a9ab81e2="">III-V Lab</p>
           </a></div>
       </div>
-      <div class="flex flex-col gap-y-content-sm justify-start items-start" data-v-a9ab81e2=""><a href="/about/"
-          class="flex justify-center text-text-primary footer-hover-active" data-v-a9ab81e2="">
-          <p class="caption-sm font-bold" data-v-a9ab81e2="">About</p>
-        </a>
-        <div class="flex flex-col gap-y-content-xs justify-start items-start" data-v-a9ab81e2=""><a
-            href="/about/researcher-profiles/" class="flex justify-center text-grey-800 footer-hover-active"
-            data-v-a9ab81e2="">
-            <p class="caption-sm font-normal" data-v-a9ab81e2="">Our people</p>
-          </a><a href="/about/the-women-driving-nokia-research-innovation/"
-            class="flex justify-center text-grey-800 footer-hover-active" data-v-a9ab81e2="">
-            <p class="caption-sm font-normal" data-v-a9ab81e2="">Our women innovators</p>
-          </a><a href="/about/locations/" class="flex justify-center text-grey-800 footer-hover-active"
-            data-v-a9ab81e2="">
-            <p class="caption-sm font-normal" data-v-a9ab81e2="">Locations</p>
-        </a><a href="/about/history/" class="flex justify-center text-grey-800 footer-hover-active" data-v-a9ab81e2="">
-          <p class="caption-sm font-normal" data-v-a9ab81e2="">History</p>
-        </a><a href="/about/awards/" class="flex justify-center text-grey-800 footer-hover-active" data-v-a9ab81e2="">
-          <p class="caption-sm font-normal" data-v-a9ab81e2="">Awards</p>
-        </a><a href="/about/press-releases/" class="flex justify-center text-grey-800 footer-hover-active"
-          data-v-a9ab81e2="">
-          <p class="caption-sm font-normal" data-v-a9ab81e2="">Press releases</p>
-        </a><a href="/about/contact/" class="flex justify-center text-grey-800 footer-hover-active" data-v-a9ab81e2="">
-          <p class="caption-sm font-normal" data-v-a9ab81e2="">Contact us</p>
-        </a></div>
-    </div>
-  </nav>
-  <div class="w-full border-t-[1px] border-grey-800" data-v-a9ab81e2=""></div>
+    </nav>
+    <div class="w-full border-t-[1px] border-grey-800" data-v-a9ab81e2=""></div>
 
     <div class="w-full text-center" data-v-a9ab81e2=""> ©2023 Nokia All rights reserved </div>
-</div></template>
+  </div>
+</template>
 <script setup>
 const cl = (event) => {
   // 获取当前
@@ -288,7 +254,8 @@ const leave = () => {
   curMenu.value = ''
 }
 </script>
-<style>ul li a {
+<style>
+ul li a {
   font-family: arial;
   color: #000000;
   text-decoration: none;
@@ -316,12 +283,11 @@ ul li a::after {
 ul a:hover::after {
   transform-origin: bottom left;
   transform: scaleX(1);
-}</style>
+}
+</style>
 
 <style>
-
-#menuToggle
-{
+#menuToggle {
   display: block;
   position: relative;
   top: 50px;
@@ -331,67 +297,63 @@ ul a:hover::after {
   user-select: none;
 }
 
-#menuToggle a
-{
+#menuToggle a {
   text-decoration: none;
   color: #232323;
-  
+
   transition: color 0.3s ease;
 }
 
-#menuToggle a:hover
-{
+#menuToggle a:hover {
   color: tomato;
 }
 
 
-#menuToggle input
-{
+#menuToggle input {
   display: block;
   width: 40px;
   height: 32px;
   position: absolute;
   top: -7px;
   left: -5px;
-  
+
   cursor: pointer;
-  
-  opacity: 0; /* hide this */
-  z-index: 2; /* and place it over the hamburger */
-  
+
+  opacity: 0;
+  /* hide this */
+  z-index: 2;
+  /* and place it over the hamburger */
+
   -webkit-touch-callout: none;
 }
 
 /*
  * Just a quick hamburger
  */
-#menuToggle span
-{
+#menuToggle span {
   display: block;
   width: 33px;
   height: 4px;
   margin-bottom: 5px;
   position: relative;
-  
+
   background: #cdcdcd;
   border-radius: 3px;
-  
+
   z-index: 1;
-  
+
   transform-origin: 4px 0px;
-  
-  transition: transform 0.5s cubic-bezier(0.77,0.2,0.05,1.0),
-              background 0.5s cubic-bezier(0.77,0.2,0.05,1.0),
-              opacity 0.55s ease;
+
+  transition: transform 0.5s cubic-bezier(0.77, 0.2, 0.05, 1.0),
+    background 0.5s cubic-bezier(0.77, 0.2, 0.05, 1.0),
+    opacity 0.55s ease;
 }
 
-#menuToggle span:first-child
-{
+#menuToggle span:first-child {
   transform-origin: 0% 0%;
 }
 
-#menuToggle span:nth-last-child(2)
-{
+#menuToggle span:nth-last-child(2) {
   transform-origin: 0% 100%;
 }
 
@@ -399,8 +361,7 @@ ul a:hover::after {
  * Transform all the slices of hamburger
  * into a crossmark.
  */
-#menuToggle input:checked ~ span
-{
+#menuToggle input:checked~span {
   opacity: 1;
   transform: rotate(45deg) translate(-2px, -1px);
   background: #232323;
@@ -409,8 +370,7 @@ ul a:hover::after {
 /*
  * But let's hide the middle one.
  */
-#menuToggle input:checked ~ span:nth-last-child(3)
-{
+#menuToggle input:checked~span:nth-last-child(3) {
   opacity: 0;
   transform: rotate(0deg) scale(0.2, 0.2);
 }
@@ -418,8 +378,7 @@ ul a:hover::after {
 /*
  * Ohyeah and the last one should go the other direction
  */
-#menuToggle input:checked ~ span:nth-last-child(2)
-{
+#menuToggle input:checked~span:nth-last-child(2) {
   transform: rotate(-45deg) translate(0, -1px);
 }
 
@@ -427,8 +386,7 @@ ul a:hover::after {
  * Make this absolute positioned
  * at the top left of the screen
  */
-#menu
-{
+#menu {
   position: absolute;
   width: 300px;
   height: 500px;
@@ -442,13 +400,12 @@ ul a:hover::after {
   overflow: scroll;
   transform-origin: 0% 0%;
   transform: translate(-100%, 0);
-  
-  transition: transform 0.5s cubic-bezier(0.77,0.2,0.05,1.0);
-  border-radius: 0  20px 20px 0;
+
+  transition: transform 0.5s cubic-bezier(0.77, 0.2, 0.05, 1.0);
+  border-radius: 0 20px 20px 0;
 }
 
-#menu li
-{
+#menu li {
   padding: 10px 0;
   font-size: 20px;
 }
@@ -456,8 +413,7 @@ ul a:hover::after {
 /*
  * And let's slide it in from the left
  */
-#menuToggle input:checked ~ ul
-{
+#menuToggle input:checked~ul {
   transform: none;
 }
 </style>

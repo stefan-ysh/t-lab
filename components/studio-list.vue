d1ff6817d4ef9ab174e4eec80027eb2d25dbaaf0
yshuai <stefan_ysh@foxmail.com>
2023-11-15 17:14:29 +0800

    feat:studio detail page

--- components/studio-list.vue
+++ components/studio-list.vue
@@ -1,30 +1,19 @@
 <template>
-    <div class="studio-container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-4 gap-y-4 justify-items-center p-5 bg-cyan-50">
-        <!-- <div class="flex flex-col w-full justify-center items-center">
-                <img class="w-full block h-auto" src="http://fakeimg.pl/4000x2000/0079D8/fff/?text=Aprcasi">
-                <h1 class="absolute">Studio 1</h1>
-            </div>
-            <div class="flex flex-col w-full justify-center items-center">
-                <img class="w-full block h-auto" src="http://fakeimg.pl/4000x2000/0079D8/fff/?text=Aprcasi">
-                <h1 class="absolute">Studio 2</h1>
-            </div>
-            <div class="flex flex-col w-full justify-center items-center">
-                <img class="w-full block h-auto" src="http://fakeimg.pl/4000x2000/0079D8/fff/?text=Aprcasi">
-                <h1 class="absolute">Studio 3</h1>
-            </div>
-            <div class="flex flex-col w-full justify-center items-center">
-                <img class="w-full block h-auto" src="http://fakeimg.pl/4000x2000/0079D8/fff/?text=Aprcasi">
-                <h1 class="absolute">Studio 4</h1>
-            </div>
-            <div class="flex flex-col w-full justify-center items-center">
-                <img class="w-full block h-auto" src="http://fakeimg.pl/4000x2000/0079D8/fff/?text=Aprcasi">
-                <h1 class="absolute">Studio 5</h1>
-            </div> -->
-        <div v-for="(studio, i) in studioList" :key="studio.img"
+    <div
+        class="studio-container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-4 gap-y-4 justify-items-center p-5 bg-cyan-50">
+        <nuxt-link
+            v-for="(studio, i) in studioList"
+            :key="studio.img"
+            :to="{
+                name: 'studio-id',
+                params: {
+                    id: studio.id
+                }
+            }"
             class="opacity-70 hover:opacity-100 cursor-pointer transition-all duration-1/2 flex flex-col w-full justify-center items-center p-5">
             <img class="w-full block h-full hover:scale-[0.98] duration-1000" :src="studio.img" />
             <div class="absolute w-1/5 text-sm lg:text-2xl text-cyan-50 text-center">{{ studio.name }}</div>
-        </div>
+        </nuxt-link>
     </div>
 </template>
 
---
+++ pages/studio/[id].vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="card bg-base-100 shadow-xl px-20 py-10">
+        <figure>
+            <img :src="curStudio.img" alt="">
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">{{ curStudio.name }}</h2>
+            <p class="first-letter:text-7xl first-letter:italic font-extralight w-full text-justify indent-10">
+                {{ curStudio.name }}: A Place for Your Dreams to Come True
+
+                {{ curStudio.name }} is dedicated to providing high-quality, personalized services in the field of art and
+                design. Our
+                mission is to help individuals and organizations achieve their goals by offering a wide range of services,
+                including graphic design, website design and development, and product design.
+                Our team of experienced designers, artists, and developers work together to create innovative solutions that
+                exceed our clients' expectations. We believe in the importance of collaboration and teamwork, and we strive
+                to achieve our goals by listening to our clients' needs and providing solutions that are tailored to their
+                specific needs.
+                {{ curStudio.name }} is located in the heart of the city, where we can draw inspiration from the diverse
+                culture
+                and
+                vibrant energy. We believe in the importance of creativity and innovation, and we encourage our clients to
+                explore their own creativity and potential.
+
+                Our services are not limited to our own city, and we are ready to serve clients from all over the world. We
+                are excited about the opportunity to work with new clients and explore new possibilities.
+
+                In conclusion, {{ curStudio.name }} is a place where dreams can come true. We are ready to take on new
+                challenges and
+                explore new opportunities. If you are looking for a partner in art and design, {{ curStudio.name }} is ready
+                to help
+                you achieve your goals.
+                {{ curStudio.name }}: A Collaborative Space for Art and Design
+
+
+                {{ curStudio.name }} is a collaborative space where art and design come together to create innovative
+                solutions
+                that
+                exceed our clients' expectations. We believe in the importance of collaboration and teamwork, and we strive
+                to achieve our goals by listening to our clients' needs and providing solutions that are tailored to their
+                specific needs.
+
+                Our team of experienced designers, artists, and developers work together to create solutions that exceed our
+                clients' expectations. They believe in the importance of collaboration and teamwork, and they strive to
+                achieve their goals by listening to their clients' needs and providing solutions that are tailored to their
+                specific needs.
+
+                {{ curStudio.name }} is located in the heart of the city, where we can draw inspiration from the diverse
+                culture
+                and
+                vibrant energy. We believe in the importance of creativity and innovation, and we encourage our clients to
+                explore their own creativity and potential.
+
+                Our services are not limited to our own city, and we are ready to serve clients from all over the world. We
+                are excited about the opportunity to work with new clients and explore new possibilities.
+
+                In conclusion, {{ curStudio.name }} is a place where dreams can come true. We are ready to take on new
+                challenges and
+                explore new opportunities. If you are looking for a partner in art and design, {{ curStudio.name }} is ready
+                to
+                help
+                you achieve your goals.</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import studioList from 'assets/data/studioList.json'
+
+const { id } = useRoute().params
+const curStudio = studioList.find((studio) => studio.id == id)
+console.log(curStudio)
+// http://fakeimg.pl/4000x2000/0079D8/fff/?text=Aprcasi
+</script>
+<style scoped></style>
